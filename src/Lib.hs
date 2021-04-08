0a57863a23527b1d1af068d968dff68e150335e1
Jacob Clark <jacob.jh.clark@googlemail.com>
2021-04-08 16:43:45 +0100

    Ordinary Least Squares implementation

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,14 +3,14 @@ module Lib
     ) where
 
 data Point = Point {
-    x :: Int,
-    y :: Int
+    x :: Double,
+    y :: Double
 } deriving Show
 
 data Line = Line (Point, Point) deriving Show
 
-data Slope = Slope Int
-data Intercept = Intercept Int
+data Slope = Slope Double
+data Intercept = Intercept Double
 
 data StraightLine = StraightLine { 
     slope :: Slope,
@@ -24,24 +24,63 @@ instance Show Intercept where
     show (Intercept b) = show b
 
 instance Show StraightLine where
-    show (StraightLine m b) = "y=" ++ (show m) ++ "x+" ++ (show b)
+    show (StraightLine m b) = formatEquation m b
+
+formatEquation :: Slope -> Intercept -> String
+formatEquation (Slope m) (Intercept b) = do
+    if (isInfinite m) == True && (isInfinite b) == True
+        then "y=x"
+        else if (isInfinite m) == True
+        then "y=x+" ++ (show b)
+            else "y=" ++ (show m) ++ "x+" ++ (show b)
 
 calculateSlope :: Line -> Slope
-calculateSlope (Line (a, b)) = Slope $ (y b - y a) `div` (x b - x a) 
+calculateSlope (Line (a, b)) = Slope $ (y b - y a) / (x b - x a) 
 
 calculateIntercept :: Point -> Slope -> Intercept
 calculateIntercept p (Slope m) = Intercept $ (y p) - (m * (x p))
 
-computeY :: Int -> Slope -> Intercept -> Int
+computeY :: Double -> Slope -> Intercept -> Double
 computeY x (Slope m) (Intercept b) = m*x+b
+
+lengthAsDouble :: [Double] -> Double
+lengthAsDouble xs = fromIntegral $ length xs
+
+mean :: [Double] -> Double
+mean xs = sum xs / lengthAsDouble xs
+
+getX :: [Point] -> [Double]
+getX xs = map (\p -> x p) xs
+
+getY :: [Point] -> [Double]
+getY xs = map (\p -> y p) xs
+
+estimateSlope :: [Double] -> [Double] -> Double
+estimateSlope xs ys = (sum $ zipWith (\x y -> (x - (mean xs)) * (y - (mean ys))) xs ys) / ((sum $ map (\x -> x - (mean xs)) xs) ** 2)
+
+estimateIntercept :: Double -> Double -> Slope -> Double
+estimateIntercept yMean xMean (Slope m)  = yMean - m * xMean
 
 lib :: IO ()
 lib = do 
-    let p1 = Point {x = 1, y = 1}
-    let p2 = Point {x = 2, y = 2}
+    let points = [ (Point {x = 1, y = 1}), (Point {x = 2, y = 2}), (Point {x = 3, y = 3}), (Point {x = 4, y = 4}), (Point {x = 5, y = 5}), (Point {x = 6, y = 6}), (Point {x = 7, y = 7}), (Point {x = 8, y = 8}), (Point {x = 9, y = 9}), (Point {x = 10, y = 10})]
 
-    let slope = calculateSlope $ Line (p1, p2)
-    let intercept = calculateIntercept p1 slope
+    let estimatedSlope = Slope $ estimateSlope (getX points) (getY points)
+    let estimatedIntercept = Intercept $ estimateIntercept (mean $ getY points) (mean $ getX points) estimatedSlope
+
+    let estimatedLine = StraightLine {
+        slope=estimatedSlope,
+        intercept=estimatedIntercept
+    }
+
+    putStrLn . show $ estimatedLine
+
+    ---------------
+    -- ONLY USEFUL FOR A STRAIGHT LINE
+    ---------------
+
+    let slope = calculateSlope $ Line ((points !! 0), (points !! 1))
+    let intercept = calculateIntercept (points !! 0) slope
 
     let straightLine = StraightLine {
         slope=slope,
